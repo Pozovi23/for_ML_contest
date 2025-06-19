3554ddcc3b624b6721023415344441848ccb38a9
Pozovi23 <gleb.zhigalov.2005@mail.ru>
2025-06-19 19:54:26 +0700

    add all

---
+++ inference.py
@@ -0,0 +1,36 @@
+from sahi import AutoDetectionModel
+from sahi.predict import get_sliced_prediction
+import cv2
+import torch
+from ultralytics import YOLO
+
+
+def inference_sahi(filepath_src, file_dir_out, filename_out, device = "cuda:0", save=False, hide_labels=True, hide_conf=True):
+    category_mapping = {"0": "person"}
+    detection_model = AutoDetectionModel.from_pretrained(
+        model_type="yolov8",
+        model_path="best.pt",
+        confidence_threshold=0.25,
+        category_mapping=category_mapping,
+        device=device,
+    )
+    result = get_sliced_prediction(
+        filepath_src,
+        detection_model,
+        slice_height=640,
+        slice_width=640,
+        overlap_height_ratio=0.2,
+        overlap_width_ratio=0.2,
+        postprocess_match_metric='IOU',
+        postprocess_type="NMS",
+        postprocess_match_threshold=0.6,
+    )
+
+    if save:
+        result.export_visuals(
+            export_dir=file_dir_out,
+            file_name=filename_out,
+            hide_labels=hide_labels,
+            hide_conf=hide_conf,
+            rect_th=2,
+        )
---
+++ main.py
@@ -0,0 +1,4 @@
+from inference import inference_sahi
+
+
+inference_sahi("test/train_BLA_0006_JPG.rf.994cd3b5e58adc3c8a7659979139b424.jpg", "./output", "out_sahi", save=True, device='cpu')
